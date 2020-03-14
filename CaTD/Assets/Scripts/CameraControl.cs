e699a857022ed351e0451d66300d074b75283b0b
Emma Parsley <emmaparsley98@gmail.com>
2020-03-14 19:16:26 +0000

    camera control

---
+++ CaTD/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Camera))]
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] float minSize;
+    [SerializeField] float maxSize;
+    [SerializeField] float speed = 150f;
+    public int invertY = -1;
+    Camera cam;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scrollValue = Input.GetAxis("Mouse ScrollWheel");
+        float camSize = cam.orthographicSize;
+        float newCamSize = camSize + (scrollValue * speed * Time.deltaTime * invertY);
+        cam.orthographicSize = Mathf.Clamp(newCamSize, minSize, maxSize);
+
+    }
+}
