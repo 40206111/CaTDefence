64f3f925449df5964dffa84db60961479ea2c3c5
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2020-03-14 22:10:17 +0000

    Grid utilities
    Conversion functions from 1D to 2D and back again.

---
+++ CaTD/Assets/Scripts/GridUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GridUtilities
+{
+    public static int gridHeight = 0;
+    public static int gridWidth = 0;
+
+    // Convert from box ID to grid coordinates
+    public static Vector2 OneToTwo(int val)
+    {
+        return new Vector2(val % gridWidth, val / gridHeight);
+    }
+    // Convert from grid coordinates to box ID
+    public static int TwoToOne(Vector2 coord)
+    {
+        return (int)(coord.x + (coord.y * gridWidth));
+    }
+}
