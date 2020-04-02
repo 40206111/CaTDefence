ca9032362d7059761d5d3219b04e6c27a73fa67c
Emma Parsley <emmaparsley98@gmail.com>
2020-04-02 21:40:03 +0100

    Simple start to AI pathing

---
+++ CaTD/Assets/Scripts/AI/AiPathing.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Path
+{
+    Square current;
+
+    Square parentSquare;
+    List<Square> futureSquares;
+}
+
+public class AiPathing
+{
+    public static Dictionary<int, Dictionary<int, Path>> CalculatePath(List<Vector2Int> enterances, List<Vector2Int> exits)
+    {
+        List<int> enterancesIndex = new List<int>();
+        List<int> exitsIndex = new List<int>();
+
+        foreach (Vector2Int coord in enterances)
+        {
+            enterancesIndex.Add(GridUtilities.TwoToOne(coord));
+        }
+
+        foreach (Vector2Int coord in exits)
+        {
+            exitsIndex.Add(GridUtilities.TwoToOne(coord));
+        }
+
+        return CalculatePath(enterancesIndex, exitsIndex);
+    }
+
+    public static Dictionary<int, Dictionary<int, Path>> CalculatePath(List<int> enterances, List<int> exits)
+    {
+        Dictionary<int, Dictionary<int, Path>> allPaths = new Dictionary<int, Dictionary<int, Path>>();
+
+        foreach (int i in enterances)
+        {
+            foreach (int j in exits)
+            {
+                List<Square> currentPath = new List<Square>();
+
+                Square startSquare = Grid.squares[i];
+                Square endSquare = Grid.squares[j];
+                Square current = startSquare;
+
+
+            }
+        }
+                     
+        return allPaths;
+    }
+
+}
