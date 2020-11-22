5a5b14d16bd3314b73de4718875810ae6498fd1b
Emma Parsley <emmaparsley98@gmail.com>
2020-11-22 14:07:22 +0000

    level editor window

---
+++ CaTD/Assets/Scripts/Levels/Editor/LevelEditorWindow.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class LevelEditorWindow : EditorWindow
+{
+    static string Path = Application.streamingAssetsPath + "/LevelData.json";
+    static AllLevels Levels;
+
+    [MenuItem("Cat Defense/Level Editor")]
+    static void Init()
+    {
+        //LevelEditorWindow window = (LevelEditorWindow)EditorWindow.GetWindow(typeof(LevelEditorWindow));
+        LevelEditorWindow window = EditorWindow.GetWindow<LevelEditorWindow>(typeof(SceneView));
+
+        window.Show();
+    }
+
+    void LoadLevels()
+    {
+        if (File.Exists(Path))
+        {
+            Debug.Log($"Loading from: {Path}");
+
+            var levelData = File.ReadAllText(Path);
+
+            Levels = JsonUtility.FromJson<AllLevels>(levelData);
+
+            if (Levels == null)
+            {
+                Levels = new AllLevels();
+            }
+        }
+        else
+        {
+            Levels = new AllLevels();
+        }
+
+        Levels.Init();
+    }
+
+    private void OnGUI()
+    {
+        if (Levels == null || Levels.Levels == null)
+        {
+            LoadLevels();
+        }
+
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Save"))
+        {
+            SaveLevels();
+        }
+
+        if (GUILayout.Button("Reload"))
+        {
+            LoadLevels();
+        }
+
+        if (GUILayout.Button("New Level"))
+        {
+            NewLevel();
+        }
+
+        GUILayout.EndHorizontal();
+
+        foreach (var level in Levels.Levels)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(level.width.ToString() + "," + level.height.ToString()) ;
+            GUILayout.EndHorizontal();
+        }
+    }
+    static void NewLevel()
+    {
+        Levels.AddLevel();
+    }
+
+    static void SaveLevels()
+    {
+        Debug.Log($"saving to: {Path}");
+
+        if (!File.Exists(Path))
+        {
+            Debug.Log("creating path");
+            Directory.CreateDirectory(Application.streamingAssetsPath);
+        }
+        File.WriteAllText(Path, JsonUtility.ToJson(Levels));
+        Debug.Log("saved");
+    }
+
+}
---
+++ CaTD/Assets/Scripts/Levels/LevelData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class LevelData
+{
+
+    public List<Vector2Int> Enterances;
+    public List<Vector2Int> Exits;
+
+    public int width;
+    public int height;
+
+    public uint TotalWaves;
+
+    //level difficulty
+    //allowed towers (by default all)
+    //enemies and their wave unlock level?
+
+}
+
+[Serializable]
+public class AllLevels
+{
+    public List<LevelData> Levels;
+
+    public void Init()
+    {
+        if (Levels == null)
+        {
+            Levels = new List<LevelData>();
+        }
+    }
+
+    public void AddLevel()
+    {
+        var newLevel = new LevelData();
+
+        Levels.Add(newLevel);
+    }
+
+}
